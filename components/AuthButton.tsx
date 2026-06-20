bf9f3996d8f8c4bd38e6e9024fc274d5119e7a00
unknown <baramgugmin@gmail.com>
2026-06-20 23:43:26 +0900

    redirectTo URL 수정

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -25,7 +25,7 @@ export default function AuthButton({ user }: Props) {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL ?? window.location.origin}/auth/callback`,
       },
     });
   };
